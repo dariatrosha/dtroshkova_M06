e0eb6f9c5bd1f7b0b5a282a9b2867cd1a49f592c
dariatrosha <dtroshkovaa@gmail.com>
2022-10-29 17:22:25 +0300

    Вариант 16.	Даны символьные файлы f и g. Записать в файл h сначала данные с файла f, потом с файла g.

---
+++ var16.py
@@ -0,0 +1,17 @@
+f=open("f.txt","w")
+f.write("Last Christmas, I gave you my heart")
+f.close()
+g=open("g.txt","w")
+g.write("But the very next day, you gave it away")
+g.close()
+h=open("h.txt","w")
+with open('f.txt', 'r') as ffile, open("h.txt", 'a') as hfile:
+    for i in ffile:
+        hfile.write(i)
+with open("g.txt", 'r') as gfile, open("h.txt", 'a') as hfile:
+    for i in gfile:
+        hfile.write(i)
+h.close()
+
+
+
